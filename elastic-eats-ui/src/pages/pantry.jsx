4b5ed49497a80f9f08db9158c1d9faa50381f559
azdoc21 <100966372+azdoc21@users.noreply.github.com>
2025-02-27 14:20:53 -0500

    Update pantry.jsx

--- elastic-eats-ui/src/pages/pantry.jsx
+++ elastic-eats-ui/src/pages/pantry.jsx
@@ -1,4 +1,4 @@
-import { Box, Heading, Text, List, ListItem, Link, Divider, Flex } from '@chakra-ui/react';
+import { Box, Heading, Text, Divider} from '@chakra-ui/react';
 import { useUser } from "../components/UserContext";
 
 const Pantry = () => {
@@ -7,33 +7,28 @@ const Pantry = () => {
     return (
         user.username ? (
             <Box maxW="600px" mx="auto" textAlign="center" p={6}>
-            <Heading size="2xl" color="accent.green" mb={4}>
-                Contact the Team
-            </Heading>
-            <Text fontSize="lg" color="text.dark" mb={6}>
-                Have any questions or feedback? Feel free to reach out to any of our team members.
-            </Text>
+                <Heading size="2xl" color="accent.green" mb={4}>
+                    Personal Pantry
+                </Heading>
+                <Text fontSize="lg" color="text.dark" mb={6}>
+                    Have any questions or feedback? Feel free to reach out to any of our team members.
+                </Text>
 
-            <Divider my={6} />
+                <Divider my={6} />
 
-            <Heading size="lg" color="text.basic" mb={4}>
-                Team Members
-            </Heading>
+                            
             </Box>
         ) : (
             <Box maxW="600px" mx="auto" textAlign="center" p={6}>
-            <Heading size="2xl" color="accent.green" mb={4}>
-                Personal Pantry
-            </Heading>
-            <Text fontSize="lg" color="text.dark" mb={6}>
-                Login in to view your personal pantry.
-            </Text>
+                <Heading size="2xl" color="accent.green" mb={4}>
+                    Personal Pantry
+                </Heading>
 
-            <Divider my={6} />
+                <Divider my={6} />
 
-            <Heading size="lg" color="text.basic" mb={4}>
-                Team Members
-            </Heading>
+                <Text fontSize="lg" color="text.dark" mb={6}>
+                    Login in to view your personal pantry.
+                </Text>
             </Box>
         )
     );
