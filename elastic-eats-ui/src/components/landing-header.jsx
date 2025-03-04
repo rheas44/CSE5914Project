ea076bd6c8bb710f98bb4356140ea6a0c12c0d20
NZarif21 <nathanzarif@gmail.com>
2025-03-04 14:39:52 -0500

    Update to landing header

--- elastic-eats-ui/src/components/landing-header.jsx
+++ elastic-eats-ui/src/components/landing-header.jsx
@@ -3,8 +3,7 @@ import { Link as RouterLink } from 'react-router-dom';
 import diningIcon from '../assets/dining.svg';
 import { useDisclosure } from '@chakra-ui/react';
 import LoginCard from './login-card';
-import { useUser } from "../components/UserContext";
-
+import { useUser } from "./UserContext";
 
 const LandingHeader = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -15,46 +14,78 @@ const LandingHeader = () => {
   };
 
   return (
-    <Flex
-      as="header"
-      justify="space-between"
-      align="center"
-      bg="primary.light"
-      p={4}
-    >
-      <Link as={RouterLink} to="/" _hover={{ textDecoration: 'none' }}>
+    user.username ? (
+      <>      
+      <Flex
+        as="header"
+        justify="space-between"
+        align="center"
+        bg="primary.light"
+        p={4}
+        >
+        <Link as={RouterLink} to="/" _hover={{ textDecoration: 'none' }}>
         <Flex align="center">
           <Image src={diningIcon} alt="Dining Icon" color='accent.green' boxSize="24px" mr={2} />
           <Heading size="md" color="accent.green" fontFamily="heading">
-            elasticeats
+          elasticeats
           </Heading>
         </Flex>
-      </Link>
-      <Flex gap={4}>
-        <Link as={RouterLink} to="/about" color="text.basic">
+        </Link>
+        <Flex gap={4}>
+          <Link as={RouterLink} to="/about" color="text.basic">
           About
-        </Link>
-        <Link as={RouterLink} to="/contact" color="text.basic">
+          </Link>
+          <Link as={RouterLink} to="/contact" color="text.basic">
           Contact
+          </Link>
+          <Link as={RouterLink} to="/pantry" color="text.basic">
+          Pantry
+          </Link>
+          <Link to="/" onClick={handleLogout}>Logout</Link>
+        </Flex>
+        </Flex>
+        <LoginCard isOpen={isOpen} onClose={onClose} />
+      </>
+    ) : (
+      <>      
+      <Flex
+        as="header"
+        justify="space-between"
+        align="center"
+        bg="primary.light"
+        p={4}
+        >
+        <Link as={RouterLink} to="/" _hover={{ textDecoration: 'none' }}>
+        <Flex align="center">
+          <Image src={diningIcon} alt="Dining Icon" color='accent.green' boxSize="24px" mr={2} />
+          <Heading size="md" color="accent.green" fontFamily="heading">
+          elasticeats
+          </Heading>
+        </Flex>
         </Link>
-        {user.username ? (
-          <>
-            <Link as={RouterLink} to="/pantry" color="text.basic">
-              Pantry
-            </Link>
-            <Link to="/" onClick={handleLogout}>
-              Logout
-            </Link>
-          </>
-        ) : (
-          <Link as={RouterLink} to="#" color="text.basic" onClick={onOpen}>
+        <Flex gap={4}>
+          <Link as={RouterLink} to="/about" color="text.basic">
+          About
+          </Link>
+          <Link as={RouterLink} to="/contact" color="text.basic">
+          Contact
+          </Link>
+          <Link as={RouterLink} to="/pantry" color="text.basic">
+          Pantry
+          </Link>
+          <Link 
+            as={RouterLink} 
+            to="#" 
+            color="text.basic" 
+            onClick={onOpen} 
+          >
             Login
           </Link>
-        )}
-      </Flex>
-      <LoginCard isOpen={isOpen} onClose={onClose} />
-    </Flex>
-  );
-};
+        </Flex>
+        </Flex>
+        <LoginCard isOpen={isOpen} onClose={onClose} />
+      </>
+    )
+  )}
 
-export default LandingHeader;
+export default LandingHeader
